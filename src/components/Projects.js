9f623f0f1e1b1d740fc15e4dfa5d7fee256ce7da
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-04-22 17:40:25 -0500

    finished for now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,14 +11,14 @@ import personaPic from '../images/persona.png';
 let ProjectsPage = (props) => {
     return(
         <div className="d-flex flex-column mb-5 mt-5">
-            <h1 className="mb-5 text-center">Projects</h1>
+            <h1 className="mb-5 text-center display-3">Projects</h1>
         
             <div className="card" style={{width: 35 + 'rem', hieght: 30 + 'rem'}}>
                 <img className="card-img-top" src={logo} alt="Card image cap" />
                 <div className="card-body">
                     <h3 className="card-title text-dark">Parenting Conundrum</h3>
-                    <p className="card-text text-dark">parenting Conundrum is my first large scale indivdual project. It was developed for parents with special needs children to able to share and recieve advice from others in the same situation.</p>
-                    <div className="d-flex">
+                    <p className="card-text text-dark">Parenting Conundrum is my first large scale indivdual project. It is a Forum App developed for parents with special needs children to able to share and recieve advice from others in the same situation.</p>
+                    <div className="d-flex justify-content-around">
                         <div>
                             <h5 className="text-center">Wireframing</h5>
                             <img src={props.wirepic} alt="" class="img-thumbnail" style={{width: 10 + 'rem', height: 10 + 'rem'}}/>
@@ -27,10 +27,11 @@ let ProjectsPage = (props) => {
                             <h5 className="text-center">Persona</h5>
                             <img src={props.personapic} alt="" class="img-thumbnail" style={{width: 10 + 'rem', height: 10 + 'rem'}}/>
                         </div>
-                        <div>
-                            <h5 className="text-center">Challenge Statement</h5>
-                            <img src={props.personapic} alt="" class="img-thumbnail" style={{width: 10 + 'rem', height: 10 + 'rem'}}/>
-                        </div>
+                    </div>
+                    <div className="mt-5">
+                        <h5 className="text-center">Challenge Statement</h5>
+                        <p className="text-dark">Parenting is one of the hardest paths one can take. How might I offer tools and a community that can equip parents with comfort, advice and knowledge that maximizes happiness for both parent and child.
+                        </p>
                     </div>
                     <div className="d-flex justify-content-center align-items-center">
                         <img src={props.logo} width="100" height="70"  alt="" />
